ee18b0b892b1e8ba064ecc2ae8e547fd0dc0494a
jaspetham <jaspertham98@gmail.com>
2022-03-10 00:31:24 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,139 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import vertex from './shaders/vertex.glsl'
+import fragment from './shaders/fragment.glsl'
+import * as dat from 'dat.gui';
+// import gsap from 'gsap';
+
+
+export default class Sketch {
+  constructor(options) {
+    this.scene = new THREE.Scene();
+
+    this.container = options.dom;
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(this.width, this.height);
+    this.renderer.setClearColor(0xeeeeee, 1); 
+    this.renderer.physicallyCorrectLights = true;
+    this.renderer.outputEncoding = THREE.sRGBEncoding;
+
+    this.container.appendChild(this.renderer.domElement);
+
+    this.camera = new THREE.PerspectiveCamera(
+      70,
+      window.innerWidth / window.innerHeight,
+      0.001,
+      1000
+    );
+
+    // var frustumSize = 10;
+    // var aspect = window.innerWidth / window.innerHeight;
+    // this.camera = new THREE.OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, -1000, 1000 );
+    this.camera.position.set(0, 0, 2);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.time = new THREE.Clock();
+    this.elapsedTime = 0;
+    this.previousTime = 0;
+
+    this.isPlaying = true;
+    this.loader = new GLTFLoader()
+    this.addObjects();
+    this.resize();
+    this.render();
+    this.setupResize();
+    // this.settings();
+  }
+
+  settings() {
+    let that = this;
+    this.settings = {
+      progress: 0,
+    };
+    this.gui = new dat.GUI();
+    this.gui.add(this.settings, "progress", 0, 1, 0.01);
+  }
+
+  setupResize() {
+    window.addEventListener("resize", this.resize.bind(this));
+  }
+
+  resize() {
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.renderer.setSize(this.width, this.height);
+    this.camera.aspect = this.width / this.height;
+
+    // image cover
+    this.imageAspect = 1;
+    let a1; let a2;
+    if(this.height/this.width>this.imageAspect) {
+      a1 = (this.width/this.height) * this.imageAspect ;
+      a2 = 1;
+    } else{
+      a1 = 1;
+      a2 = (this.height/this.width) / this.imageAspect;
+    }
+
+    this.material.uniforms.resolution.value.x = this.width;
+    this.material.uniforms.resolution.value.y = this.height;
+    this.material.uniforms.resolution.value.z = a1;
+    this.material.uniforms.resolution.value.w = a2;
+    
+    
+    this.camera.updateProjectionMatrix();
+  }
+
+  addObjects() {
+    let that = this;
+    this.material = new THREE.ShaderMaterial({
+      extensions: {
+        derivatives: "#extension GL_OES_standard_derivatives : enable"
+      },
+      side: THREE.DoubleSide,
+      uniforms: {
+        time: {value: 0 },
+        resolution: { value: new THREE.Vector4() },
+        uvRate1: {
+          value: new THREE.Vector2(1, 1)
+        }
+      },
+      vertexShader: vertex,
+      fragmentShader: fragment
+    });
+
+    this.geometry = new THREE.PlaneGeometry(1, 1, 1, 1);
+
+    this.plane = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.plane);
+  }
+
+  stop() {
+    this.isPlaying = false;
+  }
+
+  play() {
+    if(!this.isPlaying){
+      this.render()
+      this.isPlaying = true;
+    }
+  }
+
+  render() {
+    if (!this.isPlaying) return;
+    this.elapsedTime = this.time.getElapsedTime()
+    // const deltaTime = this.elapsedTime - this.previousTime
+    // this.previousTime = this.elapsedTime
+    this.material.uniforms.time.value = this.elapsedTime;
+    requestAnimationFrame(this.render.bind(this));
+    this.renderer.render(this.scene, this.camera);
+  }
+}
+
+new Sketch({
+  dom: document.getElementById("container")
+});
+
